d589cdebe9188c4eb433515c74f6cd97931e9201
LKSLukeStrike <github@spiral.levillage.org>
2025-01-06 13:49:18 +0100

    Add optional palette map to sprites, characters works now

--- Nitcha/libraries/tic/Tic.lua
+++ Nitcha/libraries/tic/Tic.lua
@@ -6,9 +6,8 @@
 --
 -- Packages
 local Nums = require("libraries/lks/Nums")
-local Classic = require("libraries/ext/Classic")
-
-
+local Classic = require("libraries/ext/Classic")                
+            
 
 --
 -- Tic
@@ -17,6 +16,8 @@ local Tic = {}
 -- Screen sizes
 Tic.SCREENW = 240 -- screen width
 Tic.SCREENH = 136 -- screen height
+-- Palette map
+Tic.PALETTEMAP = 0x3FF0 * 2 -- vram bank 1
 -- Palette colors
 Tic.COLOR00 = 00 -- black
 Tic.COLOR01 = 01 -- purple
@@ -100,7 +101,6 @@ end
 
 function Tic:logStack(...) -- add item to the log
     local _args = {...}
-    if #_args == 0 then return end
     local _item = ""
     for _, _val in ipairs(_args) do
         _item = _item.._val.." "
@@ -125,9 +125,23 @@ end
 
 
 -- Trace
-function Tic:traceTable(_table)
+function Tic:trace(...) -- trace with multiple args
+    local _args = {...}
+    local _item = ""
+    for _, _val in ipairs(_args) do
+        _val = _val or "NIL"
+        if type(_val) == "table" then
+            Tic:traceTable(_val)
+        else
+            _item = _item.._val.." "
+        end
+    end
+    trace(_item)
+end
+
+function Tic:traceTable(_table) -- trace a table keys, vals and vals types
     for _key, _val in pairs(_table) do
-        trace(_key.." = ".._val..",\t"..type(_val))
+        Tic:trace(_key, _val, type(_val))
     end
 end
 
@@ -138,22 +152,30 @@ end
 --
 local CSprite = Classic:extend() -- general sprites
 CSprite.SPRITEBANK = 0
-CSprite.sprite = 0 -- initial sprite number
-CSprite.screenx = 0 -- screen positions
-CSprite.screeny = 0
-CSprite.colorkey = Tic.COLORKEY -- default colorkey
-CSprite.scale = Tic.SPRITESCALE01 -- default scale
-CSprite.flip = Tic.DIRHLF -- all sprites are dir h left by default
-CSprite.rotate = Tic.ROTATE000 -- no rotation by default
-CSprite.width = 1 -- sprite 1x1 by default
-CSprite.height = 1
-function CSprite:draw(_sprite, _offset) -- draw a sprite
-    _sprite = _sprite or self.sprite
+function CSprite:new()
+    CSprite.super.new(self)
+    self.spritebank = CSprite.SPRITEBANK
+    self.sprite = self.spritebank -- initial sprite number
+    self.screenx = 0 -- screen positions
+    self.screeny = 0
+    self.colorkey = Tic.COLORKEY -- default colorkey
+    self.scale = Tic.SPRITESCALE01 -- default scale
+    self.flip = Tic.DIRHLF -- all sprites are dir h left by default
+    self.rotate = Tic.ROTATE000 -- no rotation by default
+    self.width = 1 -- sprite 1x1 by default
+    self.height = 1
+    -- self.palettemap -- optional palette map for swapping
+end
+
+function CSprite:draw(_offset) -- draw a sprite -- use offset for frames
     _offset = _offset or 0
     Tic:logStack("XY:", self.screenx, self.screeny)
-    Tic:logStack("SO:", _sprite, _offset)
+    Tic:logStack("SO:", self.sprite, _offset)
+    for _key, _val in pairs(self.palettemap or {}) do -- swap palette colors if any
+        poke4(Tic.PALETTEMAP + _key, _val)
+    end
     spr(
-        _sprite + _offset,
+        self.sprite + _offset,
         self.screenx,
         self.screeny,
         self.colorkey,
@@ -163,40 +185,119 @@ function CSprite:draw(_sprite, _offset) -- draw a sprite
         self.width,
         self.height
     )
+    for _key, _val in pairs(self.palettemap or {}) do -- restore palette colors if any
+        poke4(Tic.PALETTEMAP + _key, _key)
+    end
 end
 
+
 local CSpriteBG = CSprite:extend() --bg sprites aka tic tiles
+function CSpriteBG:new()
+    CSpriteBG.super.new(self)
+    Tic:trace("CSpriteBG", self.sprite, type(self))
+end
+
 
 local CSpriteFG = CSprite:extend() --fg sprites aka tic sprites
 CSpriteFG.SPRITEBANK = 256
+function CSpriteFG:new()
+    CSpriteFG.super.new(self)
+    self.spritebank = CSpriteFG.SPRITEBANK
+    self.sprite = self.spritebank
+    Tic:trace("CSpriteFG", self.sprite, type(self))
+end
+
 
 local CEntity = Classic:extend() -- general entities like places, objects, characters ...
-CEntity.worldx = 0 -- world positions
-CEntity.worldy = 0
+CEntity.WORLDX = 0
+CEntity.WORLDY = 0
+function CEntity:new()
+    CEntity.super.new(self)
+    self.worldx = CEntity.WORLDX -- world positions
+    self.worldy = CEntity.WORLDY
+    Tic:trace("CSpriteFG", self.sprite, type(self))
+end
+
 
 local CObject = CEntity:extend() -- objects
 
+
 local CCharacter = CEntity:extend() -- characters
-CCharacter:implement(nil, nil, CSpriteFG)
 CCharacter.SIZEL = 0 -- character size aka the head sprite y offset
 CCharacter.SIZEM = 1
 CCharacter.SIZES = 2
-CCharacter.size = CCharacter.SIZEM
-function CCharacter:new(_sprite, _screenx, _screeny, _size)
-    trace("CCharacter")
-    self.sprite = _sprite or self.SPRITEBANK
-    self.screenx = _screenx or 100
-    self.screeny = _screeny or 100
-    self.size = _size or self.size
+CCharacter.BODYHUMANOID = 394 -- sprite for humanoid bodies
+CCharacter.HEADSPRITE = 377 -- initial head sprite (middle)
+CCharacter.BODYPALETTEMAP = { -- example of a body palette map
+    [Tic.COLORARMOR] = Tic.COLORARMOR,
+    [Tic.COLORSHIRT] = Tic.COLORSHIRT,
+    [Tic.COLORPANTS] = Tic.COLORPANTS,
+    [Tic.COLORHANDS] = Tic.COLORHANDS,
+}
+function CCharacter:new()
+    CCharacter.super.new(self)
+    self.size = CCharacter.SIZEM
+    self.screenx = 100
+    self.screeny = 100
+    self.scale = Tic.SPRITESCALE01
+    self.headsprite = CSpriteFG()
+    self.headsprite.sprite = CCharacter.HEADSPRITE
+    self.headsprite.palettemap = {} --empty palette map by default by can be filled later
+    self.bodysprite = CSpriteFG()
+    self.bodysprite.sprite = CCharacter.BODYHUMANOID
+    self.bodysprite.palettemap = {} --empty palette map by default by can be filled later
 end
+
+function CCharacter:draw(_offset)
+    _offset = _offset or 0
+    Tic:logStack("DRAW:", self.bodysprite.sprite, self.screenx, self.screeny)
+    -- CCharacter:drawWeapon()
+    -- CCharacter:drawShield()
+    -- CCharacter._drawBody(self, _offset)
+    self:_drawBody(_offset)
+    self:_drawHead(_offset)
+    -- CCharacter._drawBody(self, _offset)
+    -- CCharacter:drawStatus()
+end
+
+function CCharacter:drawWeapon()
+    -- Tic:logStack("WEAPON:")
+end
+
+function CCharacter:drawShield()
+    -- Tic:logStack("SHIELD:")
+end
+
+function CCharacter:_drawBody(_offset)
+    Tic:logStack("BODY:", self.bodysprite.sprite)
+    self.bodysprite.screenx = self.screenx -- apply screen positions and scale to the bodysprite
+    self.bodysprite.screeny = self.screeny
+    self.bodysprite.scale = self.scale
+    self.bodysprite:draw(_offset)
+end
+
+function CCharacter:_drawHead()
+    Tic:logStack("HEAD:", self.headsprite.sprite)
+    self.headsprite.screenx = self.screenx -- apply screen positions and scale to the headsprite
+    self.headsprite.screeny = self.screeny + self.size * self.scale -- adjust the head y position
+    self.headsprite.scale = self.scale
+    self.headsprite:draw(_offset)
+end
+
+function CCharacter:drawStatus()
+    -- Tic:logStack("STATUS:")
+end
+
 
 local CPlayer = CCharacter:extend() -- player characters
-function CPlayer:new(_sprite, _screenx, _screeny, _size)
-    trace("CPlayer")
-    self.super:new(_sprite, _screenx, _screeny, _size)
+function CPlayer:new()
+    CPlayer.super.new(self)
+    Tic:trace("CPlayer", self.bodysprite, type(self))
 end
 
+
 local CNeutral= CCharacter:extend() -- neutral characters
+
 
 local CEnnemy = CCharacter:extend() -- ennemy characters
 
@@ -204,19 +305,62 @@ local CEnnemy = CCharacter:extend() -- ennemy characters
 
 -- test
 -- local Nitcha = CPlayer:extend() -- nitcha character
-local Nitcha = CPlayer(394) -- nitcha character
+local Nitcha = CPlayer() -- nitcha character
+Nitcha.screeny = 50
 Tic:traceTable(Nitcha)
--- local Nitcha2x = CPlayer(394, 150, 100) -- scale 2x
--- Nitcha2x.scale = Tic.SPRITESCALE02
+local Nitcha2x = CPlayer() -- scale 2x
+Nitcha2x.scale = Tic.SPRITESCALE02
+Nitcha2x.screenx = 150
+local Goliath = CPlayer() -- scale 2x
+Goliath.scale = Tic.SPRITESCALE02
+Goliath.screenx = 180
+Goliath.size = CCharacter.SIZEL
+Goliath.bodysprite.palettemap[Tic.COLORSHIRT] = Tic.COLOR08
+Goliath.bodysprite.palettemap[Tic.COLORPANTS] = Tic.COLOR09
+Goliath.headsprite.sprite = 381
+local Prinn = CPlayer() -- scale 2x
+Prinn.scale = Tic.SPRITESCALE02
+Prinn.screenx = 120
+Prinn.size = CCharacter.SIZES
+Prinn.bodysprite.palettemap[Tic.COLORARMOR] = Tic.COLOR07
+Prinn.bodysprite.palettemap[Tic.COLORSHIRT] = Tic.COLOR13
+Prinn.bodysprite.palettemap[Tic.COLORPANTS] = Tic.COLOR12
+Prinn.headsprite.sprite = 373
+local Prunn = CPlayer() -- scale 2x
+Prunn.scale = Tic.SPRITESCALE02
+Prunn.screenx = 90
+Prunn.size = CCharacter.SIZES
+Prunn.headsprite.palettemap[Tic.COLOR03] = Tic.COLOR01
+Prunn.headsprite.palettemap[Tic.COLOR04] = Tic.COLOR02
+Prunn.headsprite.palettemap[Tic.COLOR06] = Tic.COLOR01
+Prunn.headsprite.palettemap[Tic.COLOR05] = Tic.COLOR02
+Prunn.bodysprite.palettemap[Tic.COLORARMOR] = Tic.COLOR01
+Prunn.bodysprite.palettemap[Tic.COLORSHIRT] = Tic.COLOR13
+Prunn.bodysprite.palettemap[Tic.COLORPANTS] = Tic.COLOR12
+Prunn.headsprite.sprite = 373
+local Truduk = CPlayer() -- scale 2x
+Truduk.scale = Tic.SPRITESCALE02
+Truduk.screenx = 60
+Truduk.size = CCharacter.SIZES
+Truduk.headsprite.sprite = 369
+-- Truduk.headsprite.palettemap[Tic.COLOR03] = Tic.COLOR11
+-- Truduk.headsprite.palettemap[Tic.COLOR02] = Tic.COLOR10
+
+
 
 
 -- Drawing
 function Tic:draw()
     local _tick = Tic.Tick
-    local _frame = Nums:yes2int(_tick < 30)
+    local _frame = _tick // 30
+    local _offset = _frame * 16
     local _characters = {
         Nitcha,
         Nitcha2x,
+        Goliath,
+        Prinn,
+        Prunn,
+        Truduk,
     }
 
     Tic:logStack("T:", _tick)
@@ -224,16 +368,15 @@ function Tic:draw()
     -- Tic:logStack("NZ:", Nitcha.size)
 
     cls()
-    drawChar(Nitcha, _frame)
-    -- drawChar(Nitcha2x, _frame, 150, 100)
+
+    for _, _character in ipairs(_characters) do
+        _character:draw(_offset)
+    end
+
     Tic:logPrint()
 end
 
-function drawChar(_char, _frame, _x, _y)
-    _char.screenx = _x or _char.screenx
-    _char.screeny = _y or _char.screeny
-    _char:draw(nil, _frame * 16)
-end
+
 
 -- END
 return Tic
--- Nitcha/puppet.lua
+++ Nitcha/puppet.lua
@@ -4,12 +4,40 @@
 -- script:  lua
 
 
--- Includes
+--
+-- Wrappers for calls external to tic80
+if not trace then
+    function trace(...)
+        print("trace:", table.unpack({...}))
+    end
+end
+    
+if not spr then
+    function spr(...)
+        print("spr:", table.unpack({...}))
+    end
+end
+    
+if not cls then
+    function cls(...)
+        print("cls:", table.unpack({...}))
+    end
+end
+
+
+--
+-- Packages
 local Tic = require("libraries/tic/Tic")
 
 
+--
 -- Main
 function TIC()
-    Tic:tick()
     Tic:draw()
+    Tic:tick() -- MUST end TIC()
+end
+
+
+if not exit then -- run it from outside tic80
+    TIC()
 end
