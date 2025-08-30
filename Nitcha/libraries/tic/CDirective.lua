58164eb9cf5ab181b9c5ff7a206a43582f43c235
LKSLukeStrike <github@spiral.levillage.org>
2025-08-30 14:20:12 +0200

    WIP boards and directives

---
+++ Nitcha/libraries/tic/CDirective.lua
@@ -0,0 +1,16 @@
+require("libraries/ext/Classic")
+--
+-- CDirective
+--
+CDirective = Classic:extend() -- generic board directive
+Classic.KINDDIRECTIVE = "CDirective"
+Classic.NAMEDIRECTIVE = "CDirective"
+function CDirective:new(_argt)
+    CDirective.super.new(self, _argt)
+    self.kind = Classic.KINDDIRECTIVE
+    self.name = Classic.NAMEDIRECTIVE
+    self.boardx = 0
+    self.boardy = 0
+    self.color  = 0
+    self:argt(_argt) -- override if any
+end
